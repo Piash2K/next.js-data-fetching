79cbe971ac6802e658983a09107400e3650e92db
PIASH <zmnobab1999@gmail.com>
2025-10-28 12:04:31 +0600

    meal data fetches using useeffeect

---
+++ src/app/meals/page.jsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+export default function MealsPage() {
+  const [meals, setMeals] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    async function fetchMeals() {
+      try {
+        const res = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+        );
+        const data = await res.json();
+        setMeals(data?.meals || []);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchMeals();
+  }, [search]);
+
+  return (
+    <div className="grid grid-cols-4 gap-4 ">
+      {meals?.map((singleMeal)=>{
+        return (
+            <div key={singleMeal.idMeal} className="border border-slate-50">
+                <p>{singleMeal?.strMeal}</p>
+                <p>{singleMeal?.strInstructions}</p>
+            </div>
+        )
+      })}
+    </div>
+  );
+}
