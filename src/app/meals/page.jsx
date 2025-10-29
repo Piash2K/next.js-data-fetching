8b6a7056c9301f4d0630511c9baa9cf1364162d1
PIASH <zmnobab1999@gmail.com>
2025-10-29 17:49:32 +0600

    next js reusable search functionalites added

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,31 +1,28 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import MealSearchInput from "./components/MealSearchInput";
 
-export default function MealsPage() {
-  const [meals, setMeals] = useState([]);
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    async function fetchMeals() {
-      try {
-        const res = await fetch(
-          `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
-        );
-        const data = await res.json();
-        setMeals(data?.meals || []);
-      } catch (error) {
-        console.error(error);
-      }
+export default async function MealsPage({searchParams}) {
+    const query = await searchParams
+    // const meals =[];
+  const fetchMeals = async () => {
+    
+    try {
+      const res = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query.search}`
+      );
+      const data = await res.json();
+      // setMeals(data?.meals || []);
+      return data.meals;
+    } catch (error) {
+      console.error(error);
+      return [];
     }
-
-    fetchMeals();
-  }, [search]);
-
+  };
+  const meals = await fetchMeals ();
   return (
     <div>
-      <div className=" text-center">
-        <input className="text-black bg-amber-50" value={search} onChange={(e) => setSearch(e.target.value)} />
-      </div>
+        <div>
+            <MealSearchInput></MealSearchInput>
+        </div>
       <div className="grid grid-cols-4 gap-4 ">
         {meals?.map((singleMeal) => {
           return (
