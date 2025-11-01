32d551a3474cd6934f1771c75b426e949860e130
PIASH <zmnobab1999@gmail.com>
2025-11-01 18:20:31 +0600

    delte and patch route added

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -5,3 +5,17 @@ export async function GET(req, {params}) {
 
   return Response.json({ id});
 }
+export async function DELETE(req, {params}) {
+
+    const {id} = await params;
+    console.log(id)
+
+  return Response.json({ id});
+}
+export async function PATCH(req, {params}) {
+
+    const {id} = await params;
+    console.log(id)
+
+  return Response.json({ id});
+}
